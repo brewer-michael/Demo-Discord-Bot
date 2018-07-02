56e69b929623e236755fed9ae7de353267a2386d
brewer-michael <mb1985@gmail.com>
2018-07-02 16:57:52 -0400

    Post Status

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    client.user.setActivity('https://git.io/d.js-heroku', {type: 'WATCHING'});
+    client.user.setActivity('Being Difficult', {type: 'WATCHING'});
 });
 
 client.on('message', msg => {
