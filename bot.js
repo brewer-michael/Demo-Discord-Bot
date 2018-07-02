36d23395ef37fe20c4fe9b04962e0432712232e3
brewer-michael <mb1985@gmail.com>
2018-07-02 17:00:44 -0400

    fix "Pong" reply

--- bot.js
+++ bot.js
@@ -6,15 +6,15 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
-    if (!msg.content.startsWith(process.env.PREFIX) || !msg.guild) return;
-    const command = msg.content.split(' ')[0].substr(process.env.PREFIX.length);
-    const args = msg.content.split(' ').slice(1).join(' ');
-    if (command === 'guide') return msg.channel.send('https://git.io/d.js-heroku');
-    if (message.content.startsWith("ping")) {
-      message.channel.send("pong!");
+    //if (!msg.content.startsWith(process.env.PREFIX) || !msg.guild) return;
+    //const command = msg.content.split(' ')[0].substr(process.env.PREFIX.length);
+    //const args = msg.content.split(' ').slice(1).join(' ');
+    //if (command === 'guide') return msg.channel.send('https://git.io/d.js-heroku');
+    if (msg.content.startsWith("ping")) {
+      msg.channel.send("pong!");
     }
-    else if (command === 'invite') return msg.channel.send(process.env.INVITE);
-    else message.channel.send("I'm sorry, I didn't understand");
+    //else if (command === 'invite') return msg.channel.send(process.env.INVITE);
+    //else message.channel.send("I'm sorry, I didn't understand");
 });
 
 client.login('NDU5NzQzMzcwNTAwMjQzNDc3.Dg_SfQ.uF6U5SBE6x1rj0Zr95vxzIymowU');
